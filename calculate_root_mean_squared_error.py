2a97ab6bfbc92c168b42be2e10edfa92658e1c2e
Alper Karaca <thisiseternity12@gmail.com>
2024-11-15 23:25:33 +0300

    Calculate RMSE

---
+++ calculate_root_mean_squared_error.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def rmse(y_true, y_pred):
+    return np.sqrt(np.mean((y_pred - y_true)**2))
+
+y_true = np.array([3, -0.5, 2, 7])
+y_pred = np.array([2.5, 0.0, 2, 8])
+print(rmse(y_true, y_pred))
