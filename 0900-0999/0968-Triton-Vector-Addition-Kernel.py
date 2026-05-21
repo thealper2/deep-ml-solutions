fd282ccf8320b78a7f846f8d2dce40d2341a4d5d
Alper Karaca <thisiseternity12@gmail.com>
2026-05-21 09:13:00 +0300

    968

---
+++ 0900-0999/0968-Triton-Vector-Addition-Kernel.py
@@ -0,0 +1,21 @@
+import torch
+import triton
+import triton.language as tl
+
+@triton.jit
+def add_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
+    pid = tl.program_id(axis=0)
+    offsets = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    result = x + y
+    tl.store(output_ptr + offsets, result, mask=mask)
+
+def add(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    output = torch.empty_like(x)
+    n_elements = x.numel()
+    BLOCK_SIZE = 1024
+    grid = (triton.cdiv(n_elements, BLOCK_SIZE),)
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=BLOCK_SIZE)
+    return output
