a82b0444326aa499a15ae34d0e4ddefaaff122ef
Alper Karaca <thisiseternity12@gmail.com>
2026-06-19 20:08:34 +0300

    fix

--- projects/Build a Trainable CNN from Scratch in NumPy/055_iterate_minibatches.py
+++ projects/Build a Trainable CNN from Scratch in NumPy/055_iterate_minibatches.py
@@ -3,6 +3,8 @@ def iterate_minibatches(x, y, batch_size, seed=0):
     idx = shuffle_indices(N, seed)
 
     for start in range(0, N - batch_size + 1, batch_size):
+        if start + batch_size > N:
+            break
         end = start + batch_size
         batch_idx = idx[start:end]
         yield x[batch_idx], y[batch_idx]
