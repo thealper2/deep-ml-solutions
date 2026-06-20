ee7cbeb9c756f39f5718c234d202c50c20bf3fa5
Alper Karaca <thisiseternity12@gmail.com>
2026-06-20 22:47:06 +0300

    fix

--- projects/AlphaZero on Connect-4 from Scratch/009_four_in_a_row_diagonal_up_right.py
+++ projects/AlphaZero on Connect-4 from Scratch/009_four_in_a_row_diagonal_up_right.py
@@ -2,7 +2,6 @@ def four_in_a_row_diagonal_up_right(board):
     for row in range(3, 6):
         for col in range(4):
             if board[row, col] != 0:
-                if board[row, col] == board[row - 1, col - 1] == board[row + 2, col + 2] == board[row - 3, col - 3]:
+                if board[row, col] == board[row - 1, col + 1] == board[row - 2, col + 2] == board[row - 3, col + 3]:
                     return int(board[row, col])
-
-        return 0
+    return 0
