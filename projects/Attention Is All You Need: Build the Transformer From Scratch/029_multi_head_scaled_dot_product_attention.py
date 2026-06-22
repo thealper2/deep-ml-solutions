d7dd57eb3fac2a79e044afef02d8f0d516451dad
Alper Karaca <thisiseternity12@gmail.com>
2026-06-22 21:05:55 +0300

    multi_head_scaled_dot_product_attention

---
+++ projects/Attention Is All You Need: Build the Transformer From Scratch/029_multi_head_scaled_dot_product_attention.py
@@ -0,0 +1,21 @@
+import torch
+
+def multi_head_scaled_dot_product_attention(q_h, k_h, v_h, mask=None):
+    B, num_heads, Lq, d_k = q_h.shape
+    Lk = k_h.shape[2]
+    d_v = v_h.shape[3]
+
+    q_flat = q_h.view(B * num_heads, Lq, d_k)
+    k_flat = k_h.view(B * num_heads, Lk, d_k)
+    v_flat = v_h.view(B * num_heads, Lk, d_v)
+
+    if mask is not None:
+        mask_flat = mask.expand(B, num_heads, Lq, Lk).reshape(B * num_heads, Lq, Lk)
+    else:
+        mask_flat = None
+
+    context_flat, weights_flat = scaled_dot_product_attention(q_flat, k_flat, v_flat, mask_flat)
+
+    context = context_flat.view(B, num_heads, Lq, d_v)
+    weights = weights_flat.view(B, num_heads, Lq, Lk)
+    return context, weights
