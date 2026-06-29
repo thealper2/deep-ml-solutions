5b4ac47a1217c6cb57c1f39a8648867098af2e99
Alper Karaca <thisiseternity12@gmail.com>
2026-06-29 23:15:10 +0300

    dot_product

---
+++ projects/Flash Attention in CUDA from Scratch/006_dot_product.cpp
@@ -0,0 +1,7 @@
+__device__ float dot_product(const float* a, const float* b, int n) {
+    float sum = 0.0f;
+    for (int i = 0; i < n; i++) {
+        sum += a[i] * b[i];
+    }
+    return sum;
+}
