26c526e646c94acb5e6dd6f199b408eb1037c5aa
Alper Karaca <thisiseternity12@gmail.com>
2026-07-02 11:25:36 +0300

    fix

--- projects/Random Forest from Scratch/013_combine_predictions.py
+++ projects/Random Forest from Scratch/013_combine_predictions.py
@@ -1,6 +1,6 @@
 def combine_predictions(tree_predictions):
     n_trees, n_samples = tree_predictions.shape
-    result = np.zeros(n_sample, dtype=int)
+    result = np.zeros(n_samples, dtype=int)
 
     for j in range(n_samples):
         votes = tree_predictions[:, j]
