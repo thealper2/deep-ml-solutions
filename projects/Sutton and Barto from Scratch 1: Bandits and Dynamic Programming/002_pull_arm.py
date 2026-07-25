7975e48eb22630bd076f6b5f9ad2c03491591001
Alper Karaca <thisiseternity12@gmail.com>
2026-07-25 16:31:36 +0300

    pull_arm

---
+++ projects/Sutton and Barto from Scratch 1: Bandits and Dynamic Programming/002_pull_arm.py
@@ -0,0 +1,12 @@
+def pull_arm(true_values, action, rng):
+    """Pull one arm and return reward = true value + unit-normal noise.
+
+    Args:
+        true_values (np.ndarray): Shape (k,) true mean reward of each arm.
+        action (int): Index of the arm to pull.
+        rng (np.random.Generator): Seeded random generator for the noise.
+
+    Returns:
+        float: Stochastic reward for this pull.
+    """
+    return rng.normal(true_values[action])
