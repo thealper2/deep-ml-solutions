548922b977ae82ee7b98b625d80fb54b624f52e4
Alper Karaca <thisiseternity12@gmail.com>
2026-07-27 20:46:37 +0300

    evaluate_predictions

---
+++ projects/NumPy Text Classifier from Scratch/024_evaluate_predictions.py
@@ -0,0 +1,13 @@
+def evaluate_predictions(y_true: np.ndarray, y_pred: np.ndarray) -> dict:
+    tp, fp, tn, fn = confusion_counts(y_true, y_pred)
+    metrics = metrics_from_counts(tp, fp, tn, fn)
+    return {
+        'tp': tp,
+        'fp': fp,
+        'tn': tn,
+        'fn': fn,
+        'precision': metrics['precision'],
+        'recall': metrics['recall'],
+        'f1': metrics['recall'],
+        'accuracy': metrics['accuracy'],
+    }
