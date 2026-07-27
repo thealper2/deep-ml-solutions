7855292acdf160e3cc332bf0b8a4c15fdf67cb39
Alper Karaca <thisiseternity12@gmail.com>
2026-07-27 20:29:16 +0300

    confusion_counts

---
+++ projects/NumPy Text Classifier from Scratch/021_confusion_counts.py
@@ -0,0 +1,6 @@
+def confusion_counts(y_true: np.ndarray, y_pred: np.ndarray) -> tuple:
+    tp = int(np.sum((y_true == 1) & (y_pred == 1)))
+    fp = int(np.sum((y_true == 0) & (y_pred == 1)))
+    tn = int(np.sum((y_true == 0) & (y_pred == 0)))
+    fn = int(np.sum((y_true == 1) & (y_pred == 0)))
+    return tp, fp, tn, fn
