93c7db57da5dfdd9d7cff6ebe38d609436eff56f
Alper Karaca <thisiseternity12@gmail.com>
2026-07-28 09:34:44 +0300

    root_mean_squared_error

---
+++ projects/NumPy House Price Regression/016_root_mean_squared_error.py
@@ -0,0 +1,11 @@
+def root_mean_squared_error(y_true, y_pred):
+    """Compute root mean squared error between targets and predictions.
+
+    Args:
+        y_true (np.ndarray): Ground-truth targets, shape (N,).
+        y_pred (np.ndarray): Predicted targets, shape (N,).
+
+    Returns:
+        float: RMSE value.
+    """
+    return np.sqrt(np.mean((y_true - y_pred) ** 2))
