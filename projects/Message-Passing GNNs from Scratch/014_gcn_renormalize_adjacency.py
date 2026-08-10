d886f513a5961391776160cb6224c3f3c34eccec
Alper Karaca <thisiseternity12@gmail.com>
2026-08-10 23:49:21 +0300

    gcn_renormalize_adjacency

---
+++ projects/Message-Passing GNNs from Scratch/014_gcn_renormalize_adjacency.py
@@ -0,0 +1,16 @@
+def gcn_renormalize_adjacency(src, dst, num_nodes):
+    """Apply Kipf-Welling renormalization: self-loops then symmetric norm.
+
+    Args:
+        src: LongTensor [E] source node indices.
+        dst: LongTensor [E] destination node indices.
+        num_nodes: int, number of nodes N.
+
+    Returns:
+        src_hat: LongTensor [E + N] sources after self-loops.
+        dst_hat: LongTensor [E + N] destinations after self-loops.
+        norm_weight: FloatTensor [E + N] symmetrically normalized weights.
+    """
+    src_hat, dst_hat = add_self_loops(src, dst, num_nodes)
+    norm_weight = symmetric_normalize_edge_weights(src_hat, dst_hat, num_nodes)
+    return src_hat, dst_hat, norm_weight
