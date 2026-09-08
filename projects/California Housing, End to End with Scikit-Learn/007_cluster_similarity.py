fdd8bb0f9fb1d41af71f8f48cd9b25d81aa28502
Alper Karaca <thisiseternity12@gmail.com>
2026-09-08 20:51:31 +0300

    ClusterSimilarity

---
+++ projects/California Housing, End to End with Scikit-Learn/007_cluster_similarity.py
@@ -0,0 +1,23 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import rbf_kernel
+
+class ClusterSimilarity(BaseEstimator, TransformerMixin):
+    def __init__(self, n_clusters=10, gamma=1.0, random_state=None):
+        self.n_clusters = n_clusters
+        self.gamma = gamma
+        self.random_state = random_state
+
+    def fit(self, X, y=None, sample_weight=None):
+        self.kmeans_ = KMeans(
+            n_clusters=self.n_clusters,
+            n_init=10,
+            random_state=self.random_state,
+        ).fit(X, sample_weight=sample_weight)
+        return self
+
+    def transform(self, X):
+        return rbf_kernel(X, self.kmeans_.cluster_centers_, gamma=self.gamma)
+
+    def get_feature_names_out(self, names=None):
+        return [f"Cluster {i} similarity" for i in range(self.n_clusters)]
