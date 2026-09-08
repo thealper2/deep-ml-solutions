70e8f5dab328050a8edabe695cd43bf245619eab
Alper Karaca <thisiseternity12@gmail.com>
2026-09-08 21:01:35 +0300

    rmse

---
+++ projects/California Housing, End to End with Scikit-Learn/011_rmse.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def rmse(y_true, y_pred):
+    y_true = np.asarray(y_true, dtype=float)
+    y_pred = np.asarray(y_pred, dtype=float)
+    return float(np.sqrt(np.mean((y_true - y_pred) ** 2)))
