a455cf3b75f497b3a5710e27bd387c0988245026
Alper Karaca <thisiseternity12@gmail.com>
2026-09-12 10:46:14 +0300

    matmul_cpu

---
+++ projects/Matrix Multiplication Algorithms in CUDA/001_matmul_cpu.cu
@@ -0,0 +1,11 @@
+void matmul_cpu(const float* A, const float* B, float* C, int M, int N, int K) {
+    for (int m = 0; m < M; ++m) {
+        for (int n = 0; n < N; ++n) {
+            float acc = 0.0f;
+            for (int k = 0; k < K; ++k) {
+                acc += A[m * K + k] * B[k * N + n];
+            }
+            C[m * N + n] = acc;
+        }
+    }
+}
