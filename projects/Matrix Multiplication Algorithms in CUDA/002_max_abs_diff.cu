09c4fc43dcbecd5aa23f289ac758ee773c7d4e40
Alper Karaca <thisiseternity12@gmail.com>
2026-09-12 10:48:47 +0300

    max_abs_diff

---
+++ projects/Matrix Multiplication Algorithms in CUDA/002_max_abs_diff.cu
@@ -0,0 +1,12 @@
+#include <math.h>
+
+float max_abs_diff(const float* a, const float* b, int n) {
+    float max_diff = 0.0f;
+    for (int i = 0; i < n; ++i) {
+        float diff = fabsf(a[i] - b[i]);
+        if (diff > max_diff) {
+            max_diff = diff;
+        }
+    }
+    return max_diff;
+}
